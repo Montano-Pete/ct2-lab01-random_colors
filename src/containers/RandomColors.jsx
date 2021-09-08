37ec0515be309cd9c3d16b2272d7b859582a6a3e
Peter <pete225848@gmail.com>
2021-09-08 12:02:28 -0700

    added baby meme to prevent repeated colors

--- src/containers/RandomColors.jsx
+++ src/containers/RandomColors.jsx
@@ -3,8 +3,8 @@ import ColorSquare from '../components/square/ColorSquare';
 
 class RandomColors extends Component {
   state = {
-    color: '#FFFF00'
-  }
+    color: '#FFFF00',
+  };
 
   randomize = () => {
     const arrayOfColors = [
@@ -14,21 +14,26 @@ class RandomColors extends Component {
       '#FFD700',
       '#FFA07A',
       '#FFB6C1',
-      '#B22222'
+      '#B22222',
     ];
 
-    return arrayOfColors[Math.floor(Math.random() * arrayOfColors.length)];
-  }
+    const antiDuplicates =
+      arrayOfColors[Math.floor(Math.random() * arrayOfColors.length)];
+
+    if(antiDuplicates !== this.state.color) return antiDuplicates;
+    else
+      return 'url("https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg")';
+  };
 
   colorInterval = () => {
     setInterval(() => {
       this.setState({ color: this.randomize() });
     }, 1000);
-  }
+  };
 
   componentDidMount = () => {
     this.colorInterval();
-  }
+  };
 
   render() {
     return <ColorSquare color={this.state.color} />;
