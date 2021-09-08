4f62e92426e33167e285c1896232a5795441f95a
Peter <pete225848@gmail.com>
2021-09-08 10:04:57 -0700

    created color square component and successfully rendering on page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Square from '../colors/ColorSquare';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Square />
+    </>
+  );
 }
---
+++ src/components/colors/ColorSquare.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Square = () => {
+  return <p>Ho Ho Ho!</p>;
+};
+
+Square.propTypes = {};
+
+export default Square;
